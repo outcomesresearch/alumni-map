ac2da532005cdfacbbabb5fae62d90a373cf07fe
jeffrey <piccirilloj1@gmail.com>
2022-02-19 20:47:12 -0600

    Revert "Try to ignore large .csv file to reduce bundle size"
    This reverts commit 606b446841d5b12d7f36d9b3f7b0006ad3435522.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
-  chainWebpack: (config) => {
-    config.plugin("copy").tap((opts) => {
-      opts[0][0].ignore.push({ glob: "us_postal_codes.csv" });
-      return opts;
-    });
-  },
-};
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
