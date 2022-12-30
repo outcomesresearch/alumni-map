5de996352b0d977408d87209992e58f07d717b0c
jeffrey <piccirilloj1@gmail.com>
2022-12-30 15:29:21 -0600

    Add manual preview / embedded meta tags for Alumni Map project

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
+const webpackParams = {
+  title:
+    'Practice Locations of Washington University in St Louis Otolaryngology Graduates ',
+  description:
+    'Interactive map showing where Washington University/Barnes-Jewish Hospital Residency Program graduates are now practicing, along with current academic status',
+  site_name:
+    'Applets from Clinical Outcomes Research at Washington University in St Louis',
+  url: 'https://outcomesresearch.github.io/alumni-map/',
+}
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/alumni-map/" : "/",
-};
+  publicPath: process.env.NODE_ENV === 'production' ? '/alumni-map/' : '/',
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      Object.entries(webpackParams).forEach(([key, value]) => {
+        args[0][key] = value
+      })
+      return args
+    })
+  },
+}
